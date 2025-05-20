2643b87505a6fc1b7acf7b941b22f7e2ad0ea21d
Taanviir <66136914+Taanviir@users.noreply.github.com>
2025-05-20 22:40:14 +0400

    feat: added json parsing for theta0 theta1

--- train.py
+++ train.py
@@ -1,16 +1,31 @@
 # train.py
 
 from utils import estimate_price
+from json import load, JSONDecodeError
 
 
 def main():
+    """Estimate the price of a car based on mileage using a trained linear model."""
+
     try:
-        mileage = int(input("Enter the mileage to estimate price for: "))
+        with open("model.json") as file:
+            params = load(file)
+            theta0 = params.get("theta0", 0)
+            theta1 = params.get("theta1", 0)
+    except JSONDecodeError:
+        print("Error: invalid json file.")
+        return
+    except FileNotFoundError:
+        print("No `model.json` file found. Setting theta0 & theta1 to 0 and 0.")
+
+    try:
+        mileage = int(input("Enter the mileage of the car (in km): "))
     except ValueError:
-        print("Error: Invalid Type entered.")
+        print("Error: Invalid type entered.")
         return
 
-    price = estimate_price(mileage)
+    price = estimate_price(mileage, theta0, theta1)
+    print(f"Estimated price for a car with {mileage} km: ${price:.2f}")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,9 +1,7 @@
 # utils.py
 
 
-def estimate_price(mileage: int | float) -> float:
-    theta0 = 0
-    theta1 = 0
-
-    price = theta0 + (theta1 * mileage)
-    return price
+def estimate_price(
+    mileage: int | float, theta0: int | float, theta1: int | float
+) -> float:
+    return theta0 + (theta1 * mileage)
