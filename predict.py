0a0dcb922cb7ebcf95a0c1a03d9adad23cfb11de
Taanviir <66136914+Taanviir@users.noreply.github.com>
2025-05-22 10:49:01 +0400

    feat: updated train.py and predict.py
    - removed model.json and added it to gitignore

--- predict.py
+++ predict.py
@@ -1,16 +1,31 @@
-# predict.py
+# train.py
 
-import pandas as pd
-
-
-def ft_linear_regression():
-
-    data = pd.read_csv("data.csv")
-    print(data)
+from utils import estimate_price
+from json import load, JSONDecodeError
 
 
 def main():
-    ft_linear_regression()
+    """Estimate the price of a car based on mileage using a trained linear model."""
+
+    try:
+        with open("model.json") as file:
+            params = load(file)
+            theta0 = params.get("theta0", 0)
+            theta1 = params.get("theta1", 0)
+    except JSONDecodeError:
+        print("Error: invalid json file.")
+        return
+    except FileNotFoundError:
+        print("No `model.json` file found. Setting theta0 & theta1 to 0 and 0.")
+
+    try:
+        mileage = int(input("Enter the mileage of the car (in km): "))
+    except ValueError:
+        print("Error: Invalid type entered.")
+        return
+
+    price = estimate_price(mileage, theta0, theta1)
+    print(f"Estimated price for a car with {mileage} km: ${price:.2f}")
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -1,32 +1,15 @@
-# train.py
+# predict.py
 
-from utils import estimate_price
-from json import load, JSONDecodeError
-
-
-def main():
-    """Estimate the price of a car based on mileage using a trained linear model."""
-
-    try:
-        with open("model.json") as file:
-            params = load(file)
-            theta0 = params.get("theta0", 0)
-            theta1 = params.get("theta1", 0)
-    except JSONDecodeError:
-        print("Error: invalid json file.")
-        return
-    except FileNotFoundError:
-        print("No `model.json` file found. Setting theta0 & theta1 to 0 and 0.")
-
-    try:
-        mileage = int(input("Enter the mileage of the car (in km): "))
-    except ValueError:
-        print("Error: Invalid type entered.")
-        return
-
-    price = estimate_price(mileage, theta0, theta1)
-    print(f"Estimated price for a car with {mileage} km: ${price:.2f}")
+import pandas as pd
 
 
-if __name__ == "__main__":
-    main()
+def ft_linear_regression():
+    """ """
+
+    # get data
+    data = pd.read_csv("data.csv")
+    print(data)
+
+    # perform linear regression
+
+    # save theta0 and theta1 to model.json
